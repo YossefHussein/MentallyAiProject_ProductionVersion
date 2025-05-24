e71627cc739107d9359bafa45297856eb0e2dd87
Youssef Hussein <youssef.hussein.business@gmail.com>
2025-05-24 14:36:04 +0300

    upgrade ai package to gen ai

--- backend/infrastructure/gemini/gemini_service.js
+++ backend/infrastructure/gemini/gemini_service.js
@@ -1,35 +1,32 @@
 // import packages
-const dotenv = require("dotenv");
+const dotenv = require('dotenv')
 // make config of dotenv
-dotenv.config();
+dotenv.config()
 // import the gemini package
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenAI } = require('@google/genai')
 // import the interface
-const QuotesRepository = require("../../application/interfaces/quotes_repository");
+const QuotesRepository = require('../../application/interfaces/quotes_repository')
 // adding the api key to gemini
-const genAi = new GoogleGenerativeAI(process.env.Gemini_API_KEY);
-// set the gemini model by [genAI]
-const model = genAi.getGenerativeModel({ model: "gemini-1.5-pro" });
-
+const genAi = new GoogleGenAI({ apiKey: process.env.Gemini_API_KEY })
 class GeminiService extends QuotesRepository {
   // we use this function in controller to get quotes or advices from gemini
   // get the advice by gemini
   async getAdviceByMood(mood) {
-    // this is prompt of the gemini
+    // the prompt with mood
     const prompt = `give the current mood of the user, provide an appropriate meditation advice or mental health exercise
                      So the mood is ${mood}: 
                            {
                             "advice": "specific advice or exercises based on the user"s mood",
                             }
-                            return the json only without using keyword json and \`\`\`json  {"advice" : " content"}\`\`\` just return raw advice`;
-
-    // adding the advice or prompt to model mean create the
-    // content of gemini and return to user by [generateContent]
-    const result = await model.generateContent(prompt);
-    // return to user by [response] method
-    const response = await result.response;
-    // return the advice as text
-    return response.text();
+                            return the json only without using keyword json and \`\`\`json  {"advice" : " content"}\`\`\` just return raw advice`
+    // the 
+    const response = await genAi.models.generateContent({
+      model: 'gemini-2.0-flash',
+      contents: prompt,
+    })
+    // // return the advice as text
+    const result = await response.text
+    return result
   }
 
   // it's like the [getAdviceByMood] but without mood
@@ -41,11 +38,11 @@ class GeminiService extends QuotesRepository {
                         "noonQuote": "Your noon quote here",
                         "eveningQuote": "Your evening quote here"
                         } 
-                        return the json only without using keyword json and \`\`\`json`;
-    const result = await model.generateContent(prompt);
-    const response = await result.response;
-    return response.text();
+                        return the json only without using keyword json and \`\`\`json`
+    const result = await model.generateContent(prompt)
+    const response = await result.response
+    return response.text()
   }
 }
 
-module.exports = GeminiService;
+module.exports = GeminiService
